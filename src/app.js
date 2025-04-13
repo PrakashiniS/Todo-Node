81b82ed919b9464339863dfbfbe674c70b135bd7
PrakashiniS <prakashini004@gmail.com>
2025-04-13 14:56:53 +0530

    new file included

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const todoRouter = require("./routes/todo");
+const app = express();
+const port = 3000;
+app.use('/todo',todoRouter);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
